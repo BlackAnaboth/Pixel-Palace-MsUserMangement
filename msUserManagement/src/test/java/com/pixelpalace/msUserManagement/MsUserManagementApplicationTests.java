c77cc635843f79adaba6f515155d39e26c036191
Andrea Diaz Correia <andrea.diaz.correia@gmail.com>
2023-10-20 16:14:18 -0300

    add: initial repo :tada:

---
+++ msUserManagement/src/test/java/com/pixelpalace/msUserManagement/MsUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelpalace.msUserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
